02d8878a1b5a3b122514e30ada2945cb82b8641c
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-10 09:38:39 -0400

    Create 5.pl

---
+++ arcade/intro/5.pl
@@ -0,0 +1,9 @@
+sub shapeArea {
+    my ($n) = $_[0];
+    if ( $n == 1 ) { 
+        $area = 1; 
+        }
+    else { 
+        $area = ($n - 1) * 4 + shapeArea( $n - 1 ); 
+        }
+    return $area;
