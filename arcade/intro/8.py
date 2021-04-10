b6ae17b053889156e7bc58115ed4668da7109e72
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-10 09:39:57 -0400

    Create 8.py

---
+++ arcade/intro/8.py
@@ -0,0 +1,9 @@
+def matrixElementsSum(matrix):
+    spooked=[]
+    roomSum=0
+    for i in enumerate(matrix):
+        rooms = [x for i, x in enumerate(i[1]) if i not in spooked]
+        roomSum+=sum(rooms)
+        for j in enumerate(i[1]):
+            if j[1]==0: spooked.append(j[0]) 
+    return roomSum
