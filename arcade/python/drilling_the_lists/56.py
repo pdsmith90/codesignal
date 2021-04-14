16d7d336e008d35f18790723d77fbb5f56b56cf6
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-14 10:20:03 -0400

    Update 56.py

--- arcade/python/drilling_the_lists/56.py
+++ arcade/python/drilling_the_lists/56.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python3
 
 def pressureGauges(morning, evening):
-    return [[min([a,b]) for a,b in zip(morning,evening)],[max([a,b]) for a,b in zip(morning,evening)]]
+    #return [[min([a,b]) for a,b in zip(morning,evening)],[max([a,b]) for a,b in zip(morning,evening)]]
+    return list(zip(*[(min(x),max(x)) for x in zip(morning,evening)]))
