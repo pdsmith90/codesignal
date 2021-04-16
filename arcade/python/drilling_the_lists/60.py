f2790ce74e9fc94728bb7d5ba15f46afc426670d
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-16 13:48:21 -0400

    Update 60.py

--- arcade/python/drilling_the_lists/60.py
+++ arcade/python/drilling_the_lists/60.py
@@ -1,4 +1,7 @@
 #! /usr/bin/env python3
 
+#def prefSum(a):
+#    return [sum(a[:i]) for i in range(1,len(a)+1)]
+
 def prefSum(a):
-    return [sum(a[:i]) for i in range(1,len(a)+1)]
+    return functools.reduce(lambda x,y: x + [x[-1]+y],a,[0])[1:]
