b0a4e5015722faeba0ecbeb53b0712adf25572e8
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-18 13:14:48 -0400

    Update 61.py

--- arcade/python/drilling_the_lists/61.py
+++ arcade/python/drilling_the_lists/61.py
@@ -1,2 +1,18 @@
 #! /usr/bin/env python3
 
+
+def mathPractice(numbers):
+    return functools.reduce(lambda s, x: s*x[0]+x[1], list(zip((numbers+[0])[0::2],(numbers+[0])[1::2])),1)
+
+
+
+#def myFun(sum1,next1):
+#
+#    if count % 2 == 1:
+#        count+=1
+#        return sum1 * next1
+#        
+#    else:
+#        count+=1
+#        return sum1 + next1
+        
