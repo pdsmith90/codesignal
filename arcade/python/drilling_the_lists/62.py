f5b33b730f2b4aec242f1144e3df1559424ce76f
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-19 12:47:57 -0400

    Update 62.py

--- arcade/python/drilling_the_lists/62.py
+++ arcade/python/drilling_the_lists/62.py
@@ -2,3 +2,7 @@
 
 def checkParticipants(participants):
     return [i[0] for i in list(filter(lambda x: x[1]<x[0], enumerate(participants) )) ]
+
+#i heard you like one liners with lambdas and enumerate that also have an extra for loop in them
+
+#returns list of indices where the value is less than the index
