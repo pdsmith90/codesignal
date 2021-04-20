ef120d8d829a1506c6532fef5484d0cb6303058d
Patrick Smith <81268345+pdsmith90@users.noreply.github.com>
2021-04-20 10:20:22 -0400

    Create 64.py

---
+++ arcade/python/drilling_the_lists/64.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+def primesSum(a, b):
+    return sum( filter(isPrime, range(a,b+1)) )
+
+
+
+
+def isPrime(a):
+    # returns true/false to be used with filter
+    
+    #first 3 numbers:
+    if (a <= 1):
+        return False
+    elif (a <= 3):
+        return True
+
+    #check if divisible by 2 or 3:
+    elif (a % 2 == 0 or a % 3 == 0):
+        return False
+    
+    #loop
+    i = 5
+    while(i * i <= a):
+        if (a % i == 0 or a % (i + 2) == 0):
+            return False
+        i = i + 6
+ 
+    return True
